874114d4edbbfad83a484221b7f628d3e964b7e4
Nikolaj Hoggins <mollevip@gmail.com>
2023-03-23 15:33:18 +0100

    Cool styling for the whole team

--- profilestyling.js
+++ profilestyling.js
@@ -4,3 +4,19 @@ const tagElement = document.getElementById("utub"); //fetches the div element wi
 
 tagElement.innerText = `Crimson`; //Changes the text of the div element to Crimson.
 tagElement.style = `margin-left: -40px; color: red; font-size:20px; display: inline-block`; //Changes the styling of the div element to the specified styling.
+
+// Only runs the code if the user is on the Crimson Claw team page.
+if (window.location.href === "https://ivrl.gg/vail/teams/crimson-claw") {
+  const allCardLists = document.getElementsByClassName("player-card-list");
+
+  const mainRoster = allCardLists[0];
+
+  const players = mainRoster.getElementsByClassName("fi");
+
+  for (let i = 0; i < players.length; i++) {
+    const playerCountry = players[i];
+    playerCountry.className = `fi`;
+    playerCountry.innerText = `Crimson`; //Changes the text of the div element to Crimson.
+    playerCountry.style = `margin-left: -40px; color: red; font-size:20px; display: inline-block; width: auto`; //Changes the styling of the div element to the specified styling.
+  }
+}
