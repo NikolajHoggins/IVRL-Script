a85060f2d77f24770c331c2eb2e1bbc8b774293b
Nikolaj Hoggins <mollevip@gmail.com>
2023-03-23 15:23:41 +0100

    Pretty crimson tag

--- profilestyling.js
+++ profilestyling.js
@@ -1,3 +1,6 @@
 //Every line is documented to provide clearity, and all code is to manipulate styling of MY profile, not to maliciously alter the site.
 
-console.log("Initializing profilestyling.js");
+const tagElement = document.getElementById("utub"); //fetches the div element with the id of utub.
+
+tagElement.innerText = `Crimson`; //Changes the text of the div element to Crimson.
+tagElement.style = `margin-left: -40px; color: red; font-size:20px; display: inline-block`; //Changes the styling of the div element to the specified styling.
