582b34e14e5fddc8b212ddabb962ede906968994
Nikolaj Hoggins <mollevip@gmail.com>
2023-03-23 15:51:09 +0100

    Only keep banner code

--- profilestyling.js
+++ profilestyling.js
@@ -2,6 +2,7 @@
 
 const tagElement = document.getElementById("utub"); //fetches the div element with the id of utub.
 
+//This applies a crimson tag to my name on all pages.
 tagElement.innerText = `Crimson`; //Changes the text of the div element to Crimson.
 tagElement.style = `margin-left: -40px; color: red; font-size:20px; display: inline-block`; //Changes the styling of the div element to the specified styling.
 
@@ -10,19 +11,4 @@ if (window.location.href === "https://ivrl.gg/vail/teams/crimson-claw") {
   //banner
   document.getElementsByClassName("horizontal-gradient-bg")[0].style =
     "background:linear-gradient(0deg, rgb(19 19 19 / 66%), rgb(0 0 0 / 46%)), url(https://cdn.discordapp.com/attachments/1080141999459541092/1080490589323280405/wolf_banner.png); background-size:cover; background-position: center;";
-
-  //remove the player card changes
-
-  //   const allCardLists = document.getElementsByClassName("player-card-list");
-
-  //   const mainRoster = allCardLists[0];
-
-  //   const players = mainRoster.getElementsByClassName("fi");
-
-  //   for (let i = 0; i < players.length; i++) {
-  //     const playerCountry = players[i];
-  //     playerCountry.className = `fi`;
-  //     playerCountry.innerText = `Crimson`; //Changes the text of the div element to Crimson.
-  //     playerCountry.style = `color: red; font-size:20px; display: inline-block; width: auto`; //Changes the styling of the div element to the specified styling.
-  //   }
 }
