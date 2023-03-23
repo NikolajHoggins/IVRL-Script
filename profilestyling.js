034f5668503d6d4f9f06f866555a10649e6e2aac
Nikolaj Hoggins <mollevip@gmail.com>
2023-03-23 13:53:19 +0100

    Lesgo

---
+++ profilestyling.js
@@ -0,0 +1,3 @@
+//Every line is documented to provide clearity, and all code is to manipulate styling of MY profile, not to maliciously alter the site.
+
+console.log("Initializing profilestyling.js");
